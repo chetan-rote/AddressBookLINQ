d561cec5278eddbfefa0fa1a04f579363dd642dc
Chetan <Chetan@DESKTOP-9SU09BS>
2020-11-26 16:32:06 +0530

    Add project files.

---
+++ AddressBookLINQ/Program.cs
@@ -0,0 +1,24 @@
+﻿/***************************************************************
+ *  Purpose: This problem implements address book using LINQ.
+ *
+ *
+ *  @author  Chetan Rote
+ *  @version 1.0
+ *  @since   26-11-2020
+ ****************************************************************/
+using System;
+
+namespace AddressBookLINQ
+{
+    class Program
+    {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book using LINQ.");
+        }
+    }
+}
